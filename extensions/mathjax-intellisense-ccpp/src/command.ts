7f848d0e2dbea1f4b5cb2c9ad11add850e562043
Casper Huang <casper.w.huang@qq.com>
2025-08-21 00:41:27 +0800

    fix: commands

--- extensions/mathjax-intellisense-ccpp/src/command.ts
+++ extensions/mathjax-intellisense-ccpp/src/command.ts
@@ -2,16 +2,16 @@ import { useCommand } from 'reactive-vscode'
 import { config } from './store/shared'
 
 export function useCommands() {
-  useCommand('mathjax-intellisense-python.edit', () => {
+  useCommand('mathjax-intellisense-ccpp.edit', () => {
     config.extension.mode = 'edit'
   })
-  useCommand('mathjax-intellisense-python.view', () => {
+  useCommand('mathjax-intellisense-ccpp.view', () => {
     config.extension.mode = 'view'
   })
-  useCommand('mathjax-intellisense-python.both', () => {
+  useCommand('mathjax-intellisense-ccpp.both', () => {
     config.extension.mode = 'both'
   })
-  useCommand('mathjax-intellisense-python.toggle', () => {
+  useCommand('mathjax-intellisense-ccpp.toggle', () => {
     config.extension.mode = config.extension.mode === 'edit'
       ? 'view'
       : 'edit'
--- extensions/mathjax-intellisense-python/src/command.ts
+++ extensions/mathjax-intellisense-python/src/command.ts
@@ -2,16 +2,16 @@ import { useCommand } from 'reactive-vscode'
 import { config } from './store/shared'
 
 export function useCommands() {
-  useCommand('mathjax-intellisense-ccpp.edit', () => {
+  useCommand('mathjax-intellisense-python.edit', () => {
     config.extension.mode = 'edit'
   })
-  useCommand('mathjax-intellisense-ccpp.view', () => {
+  useCommand('mathjax-intellisense-python.view', () => {
     config.extension.mode = 'view'
   })
-  useCommand('mathjax-intellisense-ccpp.both', () => {
+  useCommand('mathjax-intellisense-python.both', () => {
     config.extension.mode = 'both'
   })
-  useCommand('mathjax-intellisense-ccpp.toggle', () => {
+  useCommand('mathjax-intellisense-python.toggle', () => {
     config.extension.mode = config.extension.mode === 'edit'
       ? 'view'
       : 'edit'
