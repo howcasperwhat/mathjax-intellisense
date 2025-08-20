eb110894b505f86626675b4ab3a5e9c14580e2b3
Casper Huang <casper.w.huang@qq.com>
2025-08-20 17:36:07 +0800

    feat(sphinx): support multiline inline  formula

--- extensions/mathjax-intellisense-python/src/machine/formula/sphinx.ts
+++ extensions/mathjax-intellisense-python/src/machine/formula/sphinx.ts
@@ -1,11 +1,11 @@
 // https://www.sphinx-doc.org/en/master/usage/restructuredtext/directives.html#directive-math
 // https://www.sphinx-doc.org/en/master/usage/restructuredtext/roles.html#role-math
 
+// TODO: use `xstate` to manage the state machine
+
+import type { Range } from 'vscode'
 import type { DocContext } from '../doc/types'
 import type { FormulaContext } from './types'
-import { Range } from 'vscode'
-
-const BLOCK_START_MARK = '.. math::'
 
 export function extract(
   texts: string[],
@@ -14,20 +14,13 @@ export function extract(
   if (texts.length === 0)
     return undefined
 
-  if (texts[0].search(/\s?/) !== 0)
-    return undefined
-
-  const first = texts.findIndex(text => text.trim().length > 0)
   const directives: string[] = []
   const regex = /^\s*:([\w-]+):(.*)$/
-  let i = first
-  while (i < texts.length) {
-    const text = texts[i]
-    const match = text.match(regex)
-    if (!match)
-      break
-    const directive = match[1]
-    directives.push(directive)
+  let i = texts.findIndex(text => text.trim().length > 0)
+  let match
+  // eslint-disable-next-line no-cond-assign
+  while ((i < texts.length) && (match = texts[i].match(regex))) {
+    directives.push(match[1])
     i++
   }
 
@@ -43,75 +36,127 @@ export function extract(
 export function parse(
   doc: DocContext,
 ): FormulaContext[] {
-  let inside = false
+  let state = 'outside' as 'outside' | 'inline' | 'block'
+  let startIndex = 0
 
-  const regex = /(?<!\\):math:`(.*?)(?<!\\)`/g
   const inlines: FormulaContext[] = []
-
   const blocks: FormulaContext[] = []
+
   const ranges: Range[] = []
   const texts: string[] = []
 
   for (let i = 0; i < doc.lines.length; i++) {
     const line = doc.lines[i]
-    if (inside) {
-      if (line.text.trim().length === 0) {
-        ranges.push(line.range)
+    if (state === 'block') {
+      const text = line.text.slice(startIndex)
+      const range = line.range.with({
+        start: line.range.start.with({
+          character: line.range.start.character + startIndex,
+        }),
+      })
+      if (text.trim().length === 0) {
         texts.push(doc.type === 'raw' ? '\\\\' : '\\\\\\\\')
+        ranges.push(range)
+        startIndex = 0
       }
-      else if (line.text.search(/\s/) === 0) {
-        ranges.push(line.range)
-        texts.push(line.text)
+      else if (text.search(/\s/) === 0) {
+        texts.push(text)
+        ranges.push(range)
+        startIndex = 0
       }
       else {
-        const text = extract(texts, doc.type)
-        text && blocks.push({
+        const extracted = extract(texts, doc.type)
+        ranges[0] = ranges[0].with({
+          start: ranges[0].start.with({
+            character: ranges[0].start.character - '.. math::'.length,
+          }),
+        })
+        extracted && blocks.push({
           ranges: [...ranges],
           type: 'sphinx' as const,
-          text,
+          text: extracted,
         })
-        ranges.length = 0
         texts.length = 0
-        inside = false
+        ranges.length = 0
+        state = 'outside'
         --i // Reprocess the current line
+        startIndex = 0
+      }
+    }
+    else if (state === 'inline') {
+      const text = line.text.slice(startIndex)
+      const range = line.range.with({
+        start: line.range.start.with({
+          character: line.range.start.character + startIndex,
+        }),
+      })
+      const index = text.search(/(?<!\\)`/)
+      if (index === -1) {
+        texts.push(text)
+        ranges.push(range)
+        startIndex = 0
+      }
+      else {
+        texts.push(text.slice(0, index))
+        ranges.push(range.with({
+          end: range.start.with({
+            character: range.start.character + index,
+          }),
+        }))
+        const extracted = texts.join(' ')
+        ranges[0] = ranges[0].with({
+          start: ranges[0].start.with({
+            character: ranges[0].start.character - ':math:`'.length,
+          }),
+        })
+        ranges[ranges.length - 1] = ranges[ranges.length - 1].with({
+          end: ranges[ranges.length - 1].end.with({
+            character: ranges[ranges.length - 1].end.character + 1,
+          }),
+        })
+        extracted && inlines.push({
+          ranges: [...ranges],
+          type: 'sphinx' as const,
+          text: extracted,
+        })
+        texts.length = 0
+        ranges.length = 0
+        state = 'outside'
+        --i // Reprocess the current line
+        startIndex += index + 1
       }
     }
     else {
-      if (line.text.startsWith(BLOCK_START_MARK)) {
-        const str = line.text.slice(BLOCK_START_MARK.length)
-        ranges.push(line.range)
-        texts.push(str)
-        inside = true
+      const text = line.text.slice(startIndex)
+      if (startIndex === 0 && text.startsWith('.. math::')) {
+        texts.length = 0
+        ranges.length = 0
+        state = 'block'
+        --i // Reprocess the current line
+        startIndex += '.. math::'.length
       }
       else {
-        let match
-        regex.lastIndex = 0
-        // eslint-disable-next-line no-cond-assign
-        while ((match = regex.exec(line.text))) {
-          const start = regex.lastIndex - match[0].length
-          const end = regex.lastIndex
-          inlines.push({
-            ranges: [new Range(
-              line.range.start.line,
-              line.range.start.character + start,
-              line.range.start.line,
-              line.range.start.character + end,
-            )],
-            type: 'sphinx' as const,
-            text: doc.type === 'raw'
-              ? match[1]
-              : match[1].replaceAll('\\\\', '\\'),
-          })
+        const index = text.indexOf(':math:`')
+        if (index === -1) {
+          state = 'outside'
+          startIndex = 0
+        }
+        else {
+          texts.length = 0
+          ranges.length = 0
+          state = 'inline'
+          --i // Reprocess the current line
+          startIndex += index + ':math:`'.length
         }
       }
     }
   }
-  if (inside) {
-    const text = extract(texts, doc.type)
-    text && blocks.push({
+  if (state === 'block') {
+    const extracted = extract(texts, doc.type)
+    extracted && blocks.push({
       ranges: [...ranges],
       type: 'sphinx' as const,
-      text,
+      text: extracted,
     })
   }
   return [...inlines, ...blocks]
